464c2451f56afea9b86c49009840ef15fc96e41e
Carley Tripp <carley.tripp@outlook.com>
2020-01-24 22:11:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ let main = document.getElementById("main");
 main.remove();
 
 let newHeader = document.createElement("h1");
-
+newHeader.setAttribute("id", "victory");
 newHeader.innerHTML = "Bob is the champion"
